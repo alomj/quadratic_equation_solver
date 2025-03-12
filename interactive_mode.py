b2c1130adece2ad4968835310e6cf2ce4149236c
vlad <vladvoytkiv321@gmail.com>
2025-03-12 16:40:57 +0200

    Implemented interactive mode with proper validation

---
+++ interactive_mode.py
@@ -0,0 +1,22 @@
+import sys
+from solver import quadratic_equation_solver
+
+def get_valid_float(prompt):
+    while True:
+        user_input = input(prompt)
+        try:
+            return float(user_input)
+        except ValueError:
+            print(f"Error. Expected a valid real number, got {user_input} instead", file=sys.stdout)
+
+def interactive_mode():
+    while True:
+        a = get_valid_float("a = ")
+        b = get_valid_float("b = ")
+        c = get_valid_float("c = ")
+
+        print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+        quadratic_equation_solver(a, b, c)
+
+if __name__ == '__main__':
+    interactive_mode()
