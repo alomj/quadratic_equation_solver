ba3ab6c3b1040dbffc786dcb4de2c66c3882d019
vlad <vladvoytkiv321@gmail.com>
2025-03-12 15:51:24 +0200

    fixed grammatical errors, removed unused variables

--- solver.py
+++ solver.py
@@ -2,16 +2,17 @@ import math
 
 
 def quadratic_equation_solver(a, b, c):
-    x1, x2 = 0, 0
     d = (b ** 2) - (4 * a * c)
+
     if d > 0:
+        print('There are 2 roots:')
         x1 = (-b + math.sqrt(d)) / (2 * a)
         x2 = (-b - math.sqrt(d)) / (2 * a)
+        print(f'X1 = {x1}, X2 = {x2}')
     elif d == 0:
+        print('There is only one root')
         x1 = (-b + math.sqrt(d)) / (2 * a)
-        return x1
+        print(f'X1 = {x1}')
+
     else:
-        print("There is no roots")
-    return x1, x2
-
-
+        print("There are no roots")
