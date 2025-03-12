e30d80705b287730ec4f063f1909fc6a1880e0bd
vlad <vladvoytkiv321@gmail.com>
2025-03-12 15:38:15 +0200

    Revert "improved print formatting"
    This reverts commit fb78934d6f5fff27aecec0e1aec1eb9030a2a3aa.

--- solver.py
+++ solver.py
@@ -2,18 +2,16 @@ import math
 
 
 def quadratic_equation_solver(a, b, c):
+    x1, x2 = 0, 0
     d = (b ** 2) - (4 * a * c)
     if d > 0:
-        print("There are 2 roots.")
         x1 = (-b + math.sqrt(d)) / (2 * a)
         x2 = (-b - math.sqrt(d)) / (2 * a)
-        print(f'x1 = {x1}\nx\n x2 = {x2}\n')
-
     elif d == 0:
-        print("There is 1 root.")
         x1 = (-b + math.sqrt(d)) / (2 * a)
-        print(f"x1 = {x1}\n")
-
+        return x1
     else:
         print("There is no roots")
+    return x1, x2
+
 
